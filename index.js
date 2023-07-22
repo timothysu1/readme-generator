3c8174e3f6c6cc250191f4dfb6e31e9b39c87ee5
Timothy <timsu100@gmail.com>
2023-07-22 12:59:06 -0700

    question add

--- index.js
+++ index.js
@@ -1,7 +1,59 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    message: '',
+    name: 'title'
+  },
+  {
+    type: 'input',
+    message: '',
+    name: 'description'
+  },
+  {
+    type: '',
+    message: '',
+    name: 'tablecontent'
+  },
+  {
+    type: '',
+    message: '',
+    name: 'installation'
+  },
+  {
+    type: '',
+    message: '',
+    name: 'usage'
+  },
+  {
+    type: '',
+    message: '',
+    name: 'liscence'
+  },
+  {
+    type: '',
+    message: '',
+    name: 'contributing'
+  },
+  {
+    type: '',
+    message: '',
+    name: 'test'
+  },
+  {
+    type: 'input',
+    message: '',
+    name: 'username'
+  },
+  {
+    type: 'input',
+    message: '',
+    name: 'email'
+  }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
