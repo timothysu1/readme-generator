8af1e87fa9aecf99b34aa0382ccac59955bc395d
Timothy <timsu100@gmail.com>
2023-07-24 01:25:47 -0700

    add all prompts

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const fs = require("fs");
 const questions = [
   {
     type: 'input',
+    message: 'What is your GitHub username?',
+    name: 'username'
+  },
+  {
+    type: 'input',
+    message: 'What is your eamil address?',
+    name: 'email'
+  },
+  {
+    type: 'input',
     message: 'What is the title of your project?',
     name: 'title'
   },
@@ -13,15 +23,23 @@ const questions = [
     message: 'Write a description for your project',
     name: 'description'
   },
-  /* {
-    type: '',
-    message: '',
-    name: 'tablecontent'
-  }, */
+  {
+    type: 'list',
+    message: 'What kind of license should your project have?',
+    name: 'liscence',
+    choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None']
+  },
   {
     type: 'input',
-    message: '',
-    name: 'installation'
+    message: 'What command should be ran to install dependencies',
+    name: 'installation',
+    default: 'npm i'
+  },
+  {
+    type: 'input',
+    message: 'What command should be ran to install tests?',
+    name: 'test',
+    default: 'npm test'
   },
   {
     type: 'input',
@@ -29,47 +47,27 @@ const questions = [
     name: 'usage'
   },
   {
-    type: 'list',
-    message: 'What kind of license should your project have?',
-    name: 'liscence'
-
-  },
-  {
     type: 'input',
-    message: '',
+    message: 'What does the user need to know about contributing to the repo?',
     name: 'contributing'
-  },
-  {
-    type: '',
-    message: '',
-    name: 'test'
-  },
-  {
-    type: 'input',
-    message: 'What is your GitHub username?',
-    name: 'username'
-  },
-  {
-    type: 'input',
-    message: 'What is your eamil address?',
-    name: 'email'
   }
+
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-inquirer.prompt(data)
-.then((response)=>{
-  console.log(response)
-  fs.writeFile(fileName,data,(err)=>
-  err ? console.error(err) : console.log('Success!')
-  );
-})
+  inquirer.prompt(data)
+    .then((response) => {
+      console.log(response)
+      fs.writeFile(fileName, data, (err) =>
+        err ? console.error(err) : console.log('Success!')
+      );
+    })
 }
 
 // TODO: Create a function to initialize app
 function init() {
-  writeToFile("README.md",questions);
+  writeToFile("README.md", questions);
 }
 
 // Function call to initialize app
